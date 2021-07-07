8dd2af102e4336b5ba185a9aedaa3ccb331726dd
Ignacio Sabatini <ignsabatini@gmail.com>
2021-07-07 12:27:04 -0300

    Adding sounds and animation to the buttons

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
+let randomChosenColour = "";
 
 nextSequence = () => {
     let randomNumber = Math.floor(Math.random()*4)
-    gamePattern.push(buttonColours[randomNumber])
+    randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+    playSound(randomChosenColour)
+}
+
+playSound = (colour) => {
+    switch (colour) {
+        case "red":
+            $("#red").addClass("pressed");
+            setTimeout(() => { $("#red").removeClass("pressed"); }, 100);
+            new Audio('sounds/red.mp3').play();
+            break;
+        case "blue":
+            $("#blue").addClass("pressed");
+            setTimeout(() => { $("#blue").removeClass("pressed"); }, 100);
+            new Audio('sounds/blue.mp3').play();
+            break;
+        case "yellow":
+            $("#yellow").addClass("pressed");
+            setTimeout(() => { $("#yellow").removeClass("pressed"); }, 100);
+            new Audio('sounds/yellow.mp3').play();
+            break;
+        case "green":
+            $("#green").addClass("pressed");
+            setTimeout(() => { $("#green").removeClass("pressed"); }, 100);
+            new Audio('sounds/green.mp3').play();
+            break;
+        default:
+            new Audio('sounds/wrong.mp3').play();
+            break;
+    }
+
 }
