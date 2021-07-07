dedb6ce584d126698f91e9e6f6f4288bac01c2a7
Ignacio Sabatini <ignsabatini@gmail.com>
2021-07-07 14:04:15 -0300

    Adding all the logic required

--- js/index.js
+++ js/index.js
@@ -1,12 +1,51 @@
-let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userChosenColour = [];
 let randomChosenColour = "";
 
+$(document).ready(function () {
+    $(document).keypress(function(event){
+        if(event.key === "a"){
+            startGame();
+        }
+    });
+    $("div[type=button]").click(function(event) {
+        let currentColour = event.currentTarget.id
+        userChosenColour.push(currentColour);
+        playSound(currentColour);
+        checkAnswer(userChosenColour.length - 1);
+
+    });
+  })
+
+startGame = () => {
+    if(gamePattern.length == 0){
+        nextSequence();
+    }
+}
+
 nextSequence = () => {
-    let randomNumber = Math.floor(Math.random()*4)
-    randomChosenColour = buttonColours[randomNumber]
-    gamePattern.push(randomChosenColour)
-    playSound(randomChosenColour)
+    userChosenColour = [];
+    let randomNumber = Math.floor(Math.random()*4);
+    randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    playSound(randomChosenColour);
+    $("#level-title").text("Level " + gamePattern.length);
+}
+
+checkAnswer = (currentLevel) => {
+
+    if(gamePattern[currentLevel] === userChosenColour[currentLevel]){
+        if(gamePattern.length === userChosenColour.length){
+            setTimeout(() => { nextSequence(); }, 1000);
+        }
+    }else{
+        playSound("wrong");
+        $("#level-title").text("GAME OVER! Press A to restart")
+        gamePattern = [];
+        userChosenColour = [];
+        randomChosenColour = "";
+    }
 }
 
 playSound = (colour) => {
@@ -32,8 +71,14 @@ playSound = (colour) => {
             new Audio('sounds/green.mp3').play();
             break;
         default:
+            $("body").addClass("game-over")
+            setTimeout(() => { $("body").removeClass("game-over") }, 200);
             new Audio('sounds/wrong.mp3').play();
             break;
     }
 
 }
+
+
+
+
